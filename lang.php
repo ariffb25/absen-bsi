66bc4a2eecf934c9e30218a9a86a5ecd24e21a02
Ariffb <ariffb755@gmail.com>
2021-01-25 14:33:18 +0700

    Create lang.php

---
+++ lang.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lang</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+
+
+
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Lang</th>
+                <th scope="col">Bahasa</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>af</td>
+                <td>Afrikaans</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>sq</td>
+                <td>Albanian</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>ar</td>
+                <td>Arabic</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>hy</td>
+                <td>Armenian</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>ca</td>
+                <td>Catalan</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>zh</td>
+                <td>Chinese</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>zh-cn</td>
+                <td>Chinese (Mandarin/China)</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>zh-tw</td>
+                <td>Chinese (Mandarin/Taiwan)</td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td>zh-yue</td>
+                <td>Cantonese</td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td>hr</td>
+                <td>Croatian</td>
+            </tr>
+            <tr>
+                <th scope="row">11</th>
+                <td>cs</td>
+                <td>Czech</td>
+            </tr>
+            <tr>
+                <th scope="row">12</th>
+                <td>da</td>
+                <td>Danish</td>
+            </tr>
+            <tr>
+                <th scope="row">13</th>
+                <td>nl</td>
+                <td>Dutch</td>
+            </tr>
+            <tr>
+                <th scope="row">14</th>
+                <td>en</td>
+                <td>English</td>
+            </tr>
+            <tr>
+                <th scope="row">15</th>
+                <td>en-au</td>
+                <td>English (Australia)</td>
+            </tr>
+            <tr>
+                <th scope="row">16</th>
+                <td>en-uk</td>
+                <td>English (United Kingdom)</td>
+            </tr>
+            <tr>
+                <th scope="row">17</th>
+                <td>en-us</td>
+                <td>English (United States)</td>
+            </tr>
+            <tr>
+                <th scope="row">18</th>
+                <td>eo</td>
+                <td>Esperanto</td>
+            </tr>
+            <tr>
+                <th scope="row">19</th>
+                <td>fi</td>
+                <td>Finnish</td>
+            </tr>
+            <tr>
+                <th scope="row">20</th>
+                <td>fr</td>
+                <td>French</td>
+            </tr>
+            <tr>
+                <th scope="row">21</th>
+                <td>de</td>
+                <td>German</td>
+            </tr>
+            <tr>
+                <th scope="row">22</th>
+                <td>el</td>
+                <td>Greek</td>
+            </tr>
+            <tr>
+                <th scope="row">23</th>
+                <td>ht</td>
+                <td>Haitian Creole</td>
+            </tr>
+            <tr>
+                <th scope="row">24</th>
+                <td>hi</td>
+                <td>Hindi</td>
+            </tr>
+            <tr>
+                <th scope="row">25</th>
+                <td>hu</td>
+                <td>Hungarian</td>
+            </tr>
+            <tr>
+                <th scope="row">26</th>
+                <td>is</td>
+                <td>Icelandic</td>
+            </tr>
+            <tr>
+                <th scope="row">27</th>
+                <td>id</td>
+                <td>Indonesian</td>
+            </tr>
+            <tr>
+                <th scope="row">28</th>
+                <td>it</td>
+                <td>Italian</td>
+            </tr>
+            <tr>
+                <th scope="row">29</th>
+                <td>ja</td>
+                <td>Japanese</td>
+            </tr>
+            <tr>
+                <th scope="row">30</th>
+                <td>ko</td>
+                <td>Korean</td>
+            </tr>
+            <tr>
+                <th scope="row">31</th>
+                <td>la</td>
+                <td>Latin</td>
+            </tr>
+            <tr>
+                <th scope="row">32</th>
+                <td>lv</td>
+                <td>Latvian</td>
+            </tr>
+            <tr>
+                <th scope="row">33</th>
+                <td>mk</td>
+                <td>Macedonian</td>
+            </tr>
+            <tr>
+                <th scope="row">34</th>
+                <td>no</td>
+                <td>Norwegian</td>
+            </tr>
+            <tr>
+                <th scope="row">35</th>
+                <td>po</td>
+                <td>Polish</td>
+            </tr>
+            <tr>
+                <th scope="row">36</th>
+                <td>pt</td>
+                <td>Portuguese</td>
+            </tr>
+            <tr>
+                <th scope="row">37</th>
+                <td>pt-br</td>
+                <td>Portuguese (Brazil)</td>
+            </tr>
+            <tr>
+                <th scope="row">38</th>
+                <td>ro</td>
+                <td>Romanian</td>
+            </tr>
+            <tr>
+                <th scope="row">39</th>
+                <td>ru</td>
+                <td>Russian</td>
+            </tr>
+            <tr>
+                <th scope="row">40</th>
+                <td>sr</td>
+                <td>Serbian</td>
+            </tr>
+            <tr>
+                <th scope="row">41</th>
+                <td>sk</td>
+                <td>Slovak</td>
+            </tr>
+            <tr>
+                <th scope="row">42</th>
+                <td>es</td>
+                <td>Spanish</td>
+            </tr>
+            <tr>
+                <th scope="row">43</th>
+                <td>es-es</td>
+                <td>Spanish (Spain)</td>
+            </tr>
+            <tr>
+                <th scope="row">44</th>
+                <td>es-us</td>
+                <td>Spanish (United States)</td>
+            </tr>
+            <tr>
+                <th scope="row">45</th>
+                <td>sw</td>
+                <td>Swahili</td>
+            </tr>
+            <tr>
+                <th scope="row">46</th>
+                <td>sv</td>
+                <td>Swedish</td>
+            </tr>
+            <tr>
+                <th scope="row">47</th>
+                <td>ta</td>
+                <td>Tamil</td>
+            </tr>
+            <tr>
+                <th scope="row">48</th>
+                <td>th</td>
+                <td>Thai</td>
+            </tr>
+            <tr>
+                <th scope="row">49</th>
+                <td>tr</td>
+                <td>Turkish</td>
+            </tr>
+            <tr>
+                <th scope="row">50</th>
+                <td>vi</td>
+                <td>Vietnamese</td>
+            </tr>
+            <tr>
+                <th scope="row">51</th>
+                <td>cy</td>
+                <td>Welsh</td>
+            </tr>
+
+        </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+
+</html>
